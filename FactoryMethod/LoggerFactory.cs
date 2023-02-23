d063dfc6e334c192c0fb8a92ac983b0b77d1c71b
Kamil Deniz <kamil07deniz@gmail.com>
2023-02-23 20:58:05 +0300

    Factory

---
+++ FactoryMethod/LoggerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new EdLogger();
+        }
+    }
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new Log4NetLogger();
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Kaydedildi");
+            ILogger logger =_loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
