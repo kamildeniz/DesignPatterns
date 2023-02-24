0e8de5648e7d48e964ae5a098909cbd532901ead
Kamil Deniz <kamil07deniz@gmail.com>
2023-02-25 02:34:24 +0300

    A message

---
+++ Adepter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adepter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Log4NetAdapter());
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+    class ProductManager
+    {
+        Ilogger _logger;
+
+        public ProductManager(Ilogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User Data.");
+            Console.WriteLine("Saved");
+        }
+    }
+    interface Ilogger
+    {
+        void Log(string message);
+    }
+    class KdLogger : Ilogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged, {0}", message);
+        }
+    }
+    //Nuget
+    class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged with Log4Net, {0}", message);
+        }
+    }
+    class Log4NetAdapter : Ilogger
+    {
+      
+
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee kamil = new Employee { Name = "Kamil" };
+            Employee ozgur = new Employee{Name = "Özgür"};
+            Employee arif = new Employee { Name = "Arif" };
+            arif.AddSubordinate(kamil);
+            arif.AddSubordinate(ozgur);
+            Contractor dursun = new Contractor { Name="Dursun"};
+            ozgur.AddSubordinate(dursun);
+            Console.WriteLine(arif.Name);
+            foreach (Employee manager in arif)
+            {
+                Console.WriteLine("  {0}",manager.Name);
+                foreach (IPerson employee in manager)
+                {
+                    Console.WriteLine("    {0}",employee.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    interface IPerson
+    {
+        string Name { get; set; }
+    }
+    class Contractor : IPerson
+    {
+        public string Name { get; set; }
+    }
+    class Employee : IPerson,IEnumerable<IPerson>
+    {
+        List<IPerson> _subordinates = new List<IPerson>();
+        public void AddSubordinate(IPerson person)
+        {
+            _subordinates.Add(person);
+        }
+        public void RemoveSubordinate(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+        public IPerson GetSubordinate(int index)
+        {
+            return _subordinates[index];
+        }
+        public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                yield return subordinate;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
